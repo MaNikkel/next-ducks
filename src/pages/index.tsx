91711cc8aa043fb98095c51346805d5f77363931
Mathias Nikkel <mathiasnikkel@gmail.com>
2020-09-19 23:50:14 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+// Styled-JSX
+export default function Home() {
+  return (
+    <div>
+        <h1 className="title">Hello World</h1>
+        <style jsx>
+            {`
+                .title{
+                    color: red;
+                }
+            `}
+        </style>
+    </div>
+  )
+}
